f43d66d32b67916fa8841409f61a98f6bbbe61d8
illusionMeister125 <faithpena@gmail.com>
2015-04-24 23:38:06 +0800

    first gui set.1

--- mainwin.py
+++ mainwin.py
@@ -18,6 +18,10 @@ class mainwin(Frame):
 		self.topwin.configure(background="sky blue")
 		self.topwin.resizable(width=FALSE, height=FALSE) 
 		self.topwin.protocol("WM_DELETE_WINDOW", quit)
-	#	self.initialize()
-	#def initialize(self):
-		
+		self.initialize()
+def initialize(self):
+		self.topFrameStyle = Style()
+		self.topFrameStyle.configure("top.TFrame", background="white")
+		self.topFrame = Frame(self.topwin, padding = "20 20 20 20", style = "desc.TFrame")
+		#self.topFrame.grid(row = 0, column = 0, rowspan = 3, columnspan = 3, padx = 5, pady = 5)
+		self.topFrame.pack(side = TOP)
