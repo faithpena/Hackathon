b8596233679cee3b247f7070fd92c70d0d5a8ca4
Aliya Miranda <miranda.aliya@gmail.com>
2015-04-25 05:51:18 +0800

    finished???

--- control.py
+++ control.py
@@ -9,56 +9,72 @@ class Control:
 		self.dist_data = {}
 		self.budget_data = {}
 		self.gas_list = {}
-	def set_car_data(self, name, cylinder, torque, hp, weight, engine):
+		self.gas_list = create_gas_list()
+	def set_car_data(self, name, cylinder, mpg, hp, weight, engine):
 		data = {}
 		data['name'] = name
-<<<<<<< HEAD
-<<<<<<< HEAD
-		data['cylinder'] = ord(cylinder) - ord(0)
-		data['torque'] = ord(torque) - ord(0)
-		data['hp'] = ord(hp) - ord(0)
-		data['weight'] = ord(weight) - ord(0)
-		data['engine'] = engine
-		self.car_data = data
-		print sel.car_data
-=======
 		data['cylinder'] = cylinder
-		data['torque'] = torque
+		data['mpg'] = mpg
 		data['hp'] = hp
 		data['weight'] = weight
 		data['engine'] = engine
 		self.car_data = data
 		print self.car_data
->>>>>>> eb93270308c2fa499dee2b4973837475834c7e8b
-	def set_dist_data(self, dist, gasType):
-=======
-		data['cylinder'] = int(cylinder)
-		data['torque'] = int(torque)
-		data['hp'] = int(hp)
-		data['weight'] = int(weight)
-		data['engine'] = engine
-		self.car_data = data
-		print sel.car_data
-	def set_dist_data(self, car_name, dist, gasType):
->>>>>>> aa
+	def set_dist_data(self, car_name, dist, luggage, gasType):
 		data = {}
 		data['car_name'] = car_name
 		data['distance'] = int(dist)
+		data['luggage'] = int(luggage)
 		data['gas_type'] = gasType
 		self.dist_data = data
 		print self.dist_data
-	def set_budget_data(self, car_name, money, gasType):
+	def set_budget_data(self, car_name, money, luggage, gasType):
 		data = {}
 		data['car_name'] = car_name
 		data['money'] = int(money)
+		data['luggage'] = int(luggage)
 		data['gas_type'] = gasType
 		self.budget_data = data
 		print self.budget_data
 	def get_car_data(self):
 		return self.get_car_list
-	def create_car(self, car_data):
+	def create_car(self):
 		car = Car()
 		car.set_input(self.car_data)
 		self.car_list[car.get_name()] = car
+	def create_gas_list(self):
+		gas = Gas()
+		gas.set('Gasoline', 15)
+		self.gas_list['Gasoline'] = gas
+		gas.set('Diesel', 16)
+		self.gas_list['Diesel'] = gas
+		gas.set('Petrol', 17)
+		self.gas_list['Petrol'] = gas
+		gas.set('Compressed Natural Gas', 18)
+		self.gas_list['Compressed Natural Gas'] = gas
+		gas.set('Ethenol', 19)
+		self.gas_list['Ethenol'] = gas
 	def get_cars(self):
 		return self.car_list
+	def compute_budget(self):
+		car = self.car_list[self.budget_data['car_name']]
+		mpg = car.get_mpg()
+		mpg = mpg/(1+int(self.budget_data['luggage']))
+		kpl = mpg * 1.609344 / 3.78541178
+		gas_cost = self.gas_list[self.budget_data['gas_type']].get_cost()
+		money = int(self.budget_data['money'])
+		gas_amt = money/gas_cost
+		distance = kpl*gas_amt
+		output = {'Liters': gas_amt, 'Distance': distance}
+		return output
+	def compute_distance(self):
+		car = self.car_list[self.dist_data['car_name']]
+		mpg = car.get_mpg()
+		mpg = mpg/(1+int(self.budget_data['luggage']))
+		kpl = mpg * 1.609344 / 3.78541178
+		gas_cost = self.gas_list[self.budget_data['gas_type']].get_cost()
+		distance = int(self.budget_data['distance'])
+		gas_amt = distance*(1/kpl)
+		money = gas_cost*gas_amt
+		output = {'Liters': gas_amt, 'Cost': money}
+		return output
--- temp_main.py
+++
@@ -1,8 +0,0 @@
-from control import*
-
-def main():
-	control = Control()
-	data = {'name': 'Family Car', 'weight': 100, 'hp': 120, 'cylinder': 4, 'torque': 10, 'engine': 2}
-	control.create_car(data)
-	cars = control.get_cars()
-	print cars[0].get_name, cars[0].get_weight
