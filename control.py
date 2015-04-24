58d76a834f68a80ea59b9896a8a1e8ab6f703093
illusionMeister125 <faithpena@gmail.com>
2015-04-25 05:56:44 +0800

    semi-finished gui

--- control.py
+++ control.py
@@ -12,29 +12,31 @@ class Control:
 		data = {}
 		data['name'] = name
 		data['cylinder'] = int(cylinder)
-		data['torque'] = ord(torque) - ord(0)
-		data['hp'] = ord(hp) - ord(0)
-		data['weight'] = ord(weight) - ord(0)
+		data['torque'] = int(torque)
+		data['hp'] = int(hp)
+		data['weight'] = int(weight)
 		data['engine'] = engine
 		self.car_data = data
 		print self.car_data
+		self.create_car()
 	def set_dist_data(self, dist, gasType):
 		data = {}
-		data['distance'] = ord(dist) - ord(0)
+		data['distance'] = int(dist)
 		data['gas_type'] = gasType
 		self.dist_data = data
 		print self.dist_data
 	def set_budget_data(self, money, gasType):
 		data = {}
-		data['money'] = ord(money) - ord(0)
+		data['money'] = int(money)
 		data['gas_type'] = gasType
 		self.budget_data = data
 		print self.budget_data
 	def get_car_data(self):
 		return self.get_car_list
-	def create_car(self, car_data):
+	def create_car(self):
 		car = Car()
 		car.set_input(self.car_data)
 		self.car_list[car.get_name()] = car
 	def get_cars(self):
+		print self.car_list.keys()
 		return self.car_list.keys()
